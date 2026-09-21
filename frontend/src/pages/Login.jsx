029fc2034d7cd8695a0015d25f071eca85e535f3
Gowtham-Mani-24 <workspaceofgowtham@gmail.com>
2026-09-21 21:21:51 +0530

    updated the auth pages in UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,79 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../api/authApi";
+
 export const Login = ()=>{
+
+    const navigate = useNavigate();
+    const [formData,setFormData] = useState({
+        email:"",
+        password:""
+    })
+    const [loading,setLoading] = useState(false);
+    const [error,setError] = useState('');
+
+    const handleChange = (e)=>{
+        const {name,value} = e.target;
+        setFormData({
+            ...formData,
+            [name]:value
+        })
+    }
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+            setError('');
+            setLoading(true);
+        try {
+            const data = await loginUser(formData);
+            localStorage.setItem('token',data.token);
+            localStorage.setItem('user',JSON.stringify(data.user));
+            
+            navigate('/dashboard');
+        } catch (error) {
+            setError(
+                error.response?.data?.message ||
+                "login failed"
+            )
+        }
+        finally{
+            setLoading(false);
+        }
+    }
     return (
+        <>
         <div>
             <h1>Login Page</h1>
+            {error && <p>{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input 
+                    type="email"
+                    name="email"
+                    value={formData.value}
+                    onChange={handleChange}
+                    required
+                    />
+                </div>
+                <br />
+                <div>
+                    <label>Password:</label>
+                    <input 
+                    type="password"
+                    name="password"
+                    value={formData.value}
+                    onChange={handleChange}
+                    required
+                    />
+                </div>
+                <br />
+                <button type="Submit" disabled={loading}>
+                    {loading ? "logging in..." : "Login"}
+                </button>
+            </form>
         </div>
+        </>
     )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,96 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { registerUser } from "../api/authApi";
+
 const Register = () => {
+
+    const navigate = useNavigate();
+
+    const [formData,setFormData]=useState({
+        name:"",
+        email:"",
+        password:""
+    })
+
+    const [error,setError] = useState('');
+    const [loading,setLoading] = useState(false);
+
+    const handleChange = (e)=>{
+        const {name,value} = e.target;
+
+        setFormData({
+            ...formData,
+            [name]:value
+        });
+    }
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+
+        setError('');
+        setLoading(true);
+
+        try {
+            await registerUser(formData);
+
+            navigate('/login');
+        } catch (error) {
+            setError(
+                error.response?.data?.message ||
+                "registration Failed"
+            )
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+
     return (
+        <>
         <div>
             <h1>Register Page</h1>
+            {error &&<p>{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Name:</label>
+                    <input 
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <br />
+                <div>
+                    <label>Email:</label>
+                    <input 
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <br />
+                <div>
+                    <label>Password:</label>
+                    <input 
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <br />
+                <button type="submit" disabled={loading}>
+                    {loading ? "Registering..." : "Register"}
+                </button>
+            </form>
         </div>
+        </>
     );
 };
 
